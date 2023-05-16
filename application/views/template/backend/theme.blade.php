d13c8e9a35027531ff59d2399698bc27130da1ef
theAliyyahm <maftukhatulaliyyah@gmail.com>
2023-05-16 20:58:37 +0700

    perubahan dan tambah fitur edit profile

--- application/views/template/backend/theme.blade.php
+++ application/views/template/backend/theme.blade.php
@@ -17,7 +17,7 @@
     
 
 <link href="https://getbootstrap.com/docs/5.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <!-- Favicons -->
 <link rel="apple-touch-icon" href="https://getbootstrap.com/docs/5.3/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
 <link rel="icon" href="https://getbootstrap.com/docs/5.3/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
@@ -187,6 +187,12 @@
             <a class="nav-link" href="{{ site_url('backend/dashboard/table') }}">
               <span data-feather="file" class="align-text-bottom"></span>
               Table
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="{{ site_url('backend/user/profile') }}">
+            <i class="bi bi-person-circle" style="margin-right: 4px;"></i>
+              Update Profile
             </a>
           </li>
         </ul>
